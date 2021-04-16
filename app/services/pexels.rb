c70a10930ceba95037214a957732b5449ad858e7
René Koller <r.koller@me.com>
2021-04-16 09:15:22 +0200

    load more results, show some stats

--- app/services/pexels.rb
+++ app/services/pexels.rb
@@ -10,31 +10,56 @@ class Pexels
     )
   end
 
+  def search_until(query, filter_tags, boost_tags, limit = 80)
+    all_photos = []
+    remaining_results = 10_000
+    total_results = 10_000
+    page = 1
+
+    while all_photos.size < 80 && remaining_results > 0
+      result_photos, total_results = Pexels.new.search(query, filter_tags, boost_tags, page)
+
+      if result_photos&.any?
+        all_photos = all_photos + result_photos
+        remaining_results = total_results - result_photos.size
+      end
+
+      remaining_results = 0 if result_photos.empty?
+
+      page += 1
+    end
+
+    [all_photos, total_results, page - 1]
+  end
+
+
   def search(query, filter_tags, boost_tags, page = 1)
     response = client.get("search") do |req|
       req.params[:query] = query
       req.params[:tags] = 1
       req.params[:page] = page
-      req.params[:per_page] = 30
+      req.params[:per_page] = 80
       req.params[:orientation] = :landscape
     end
 
-    photos = JSON.parse(response.body, object_class: OpenStruct).photos
+    data = JSON.parse(response.body, object_class: OpenStruct)
+    photos = data.photos
+    total_results = data.total_results
 
-    if filter_tags.any?
+    if filter_tags.any? && photos&.any?
       photos = photos.select do |photo|
         tags = photo.tags.map { |tag| tag.split(" ") }.flatten
         (tags & filter_tags).empty?
       end
     end
 
-    if boost_tags.any?
+    if boost_tags.any? && photos&.any?
       photos = photos.select do |photo|
         tags = photo.tags.map { |tag| tag.split(" ") }.flatten
         (tags & boost_tags).any?
       end
     end
 
-    photos
+    [photos, total_results]
   end
 end
